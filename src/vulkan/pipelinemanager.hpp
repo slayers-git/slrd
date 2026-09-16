8529fabf9743ad5cac2b546578d4050f93feab85
iTSL4y3R <its.sl4y3r@gmail.com>
2026-09-17 02:54:00 +0900

    Reworked PipelineManager::getOrCreatePipeline()
    - PipelineManager::getOrCreatePipeline() now doesn't create the pipeline
      inside of its critical section.
    - PipelineManager::m_hashToPipelineLayoutInfo hashmap now doesn't use
      atomics for reference counting since it's always read/written to while
      holding a mutex.

--- src/vulkan/pipelinemanager.hpp
+++ src/vulkan/pipelinemanager.hpp
@@ -88,15 +88,21 @@ namespace slrd {
     private:
         VKDevice *m_device;
 
+        /* TODO: rwlock */
         std::mutex m_pipelineMtx;
         std::mutex m_pipelineLayoutMtx;
 
         std::unordered_map<PipelineStateHash, VkPipeline>
             m_stateRpToPipeline;
 
-        /* Stores the PipelineLayout itself along with the reference counting */
+        struct PipelineLayoutInfoRef {
+            PipelineLayoutInfo info;
+            uint32_t counter = 1;
+        };
+
         std::unordered_map<PipelineLayoutInfoHash,
-            std::pair<PipelineLayoutInfo, std::atomic<uint32_t>>> m_hashToPipelineLayoutInfo;
+            PipelineLayoutInfoRef> m_hashToPipelineLayoutInfo;
+
         std::unordered_map<PipelineLayoutInfoHash,
             std::unique_ptr<VKPipelineLayout>> m_hashToPipelineLayout;
 
@@ -104,6 +110,8 @@ namespace slrd {
 
         VkPipeline createPipelineForRenderPass (const VKPipelineState& state,
                 VKRenderPass *rp);
+
+        VKPipelineLayout *getOrGrabPipelineLayout(PipelineLayoutInfoHash, bool bump);
 
     public:
         PipelineManager (VKDevice *device);
